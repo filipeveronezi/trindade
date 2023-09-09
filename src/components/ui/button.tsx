513144fca7573651b4f33d4887c16763e7a8591d
Filipe Veronezi <filipeseidi@hotmail.com>
2023-09-08 21:00:18 -0300

    refactor: update button ui

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import * as React from 'react'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-full text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-400 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-zinc-950 dark:focus-visible:ring-zinc-800',
+  'inline-flex items-center justify-center rounded-full text-sm font-semibold ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-400 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-zinc-950 dark:focus-visible:ring-zinc-800',
   {
     variants: {
       variant: {
@@ -22,7 +22,7 @@ const buttonVariants = cva(
         link: 'text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50'
       },
       size: {
-        default: 'h-10 px-4 py-2',
+        default: 'h-10 px-5 py-3',
         sm: 'h-9 rounded-full px-3',
         lg: 'h-11 rounded-full px-8',
         icon: 'h-10 w-10'
