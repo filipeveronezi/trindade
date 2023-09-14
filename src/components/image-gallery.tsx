6f171b48f851cd7b6b83a43cc7a85212f45d5212
Filipe Veronezi <filipeseidi@hotmail.com>
2023-09-14 03:08:44 -0300

    fix: images on mobile

--- src/components/image-gallery.tsx
+++ src/components/image-gallery.tsx
@@ -3,54 +3,54 @@ import Image from 'next/image'
 export function ImageGallery() {
   return (
     <section className="grid grid-cols-2 gap-4">
-      <div className="grid gap-4">
+      <div className="grid max-w-xs gap-4">
         <Image
-          className="h-full w-full rounded-xl object-cover"
+          className="aspect-square rounded-xl object-cover"
           priority
           src="/galeria-3.jpg"
           alt="Momento do café da Igreja Batista da Trindade"
-          width={250}
-          height={250}
+          width={1000}
+          height={1000}
         />
         <Image
-          className="h-full w-full rounded-xl object-cover"
+          className="aspect-[9/16] rounded-xl object-cover"
           priority
           src="/galeria-2.jpg"
           alt="Quadros decorativos da Igreja Batista da Trindade"
-          width={250}
-          height={250}
+          width={1000}
+          height={1000}
         />
 
         <Image
-          className="h-full w-full rounded-xl object-cover"
+          className="aspect-square rounded-xl object-cover"
           src="/galeria-4.jpg"
           alt="Membro da Igreja Batista da Trindade ajudando com a limpeza"
-          width={250}
-          height={250}
+          width={1000}
+          height={1000}
         />
       </div>
-      <div className="grid gap-4">
+      <div className="grid max-w-xs gap-4">
         <Image
-          className="h-full w-full rounded-xl object-cover"
+          className="aspect-[9/16] rounded-xl object-cover"
           priority
           src="/galeria-1.jpg"
           alt="Pastor Pedro lecionando na Escola Bíblica Dominical"
-          width={250}
-          height={250}
+          width={1000}
+          height={1000}
         />
         <Image
-          className="h-full w-full rounded-xl object-cover"
+          className="aspect-square rounded-xl object-cover"
           src="/galeria-6.jpg"
           alt="A Bíblia Sagrada"
-          width={250}
-          height={250}
+          width={1000}
+          height={1000}
         />
         <Image
-          className="h-full w-full rounded-xl object-cover"
+          className="aspect-square rounded-xl object-cover"
           src="/galeria-5.jpg"
           alt="Culto da Igreja Batista da Trindade"
-          width={250}
-          height={250}
+          width={1000}
+          height={1000}
         />
       </div>
     </section>
