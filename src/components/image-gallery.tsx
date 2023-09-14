865a48267891556e780531330fc17ade7308eec8
Filipe Veronezi <filipeseidi@hotmail.com>
2023-09-14 10:28:34 -0300

    fix: image size

--- src/components/image-gallery.tsx
+++ src/components/image-gallery.tsx
@@ -9,24 +9,24 @@ export function ImageGallery() {
           priority
           src="/galeria-3.jpg"
           alt="Momento do café da Igreja Batista da Trindade"
-          width={1000}
-          height={1000}
+          width={300}
+          height={300}
         />
         <Image
           className="aspect-[9/16] rounded-xl object-cover"
           priority
           src="/galeria-2.jpg"
           alt="Quadros decorativos da Igreja Batista da Trindade"
-          width={1000}
-          height={1000}
+          width={300}
+          height={300}
         />
 
         <Image
           className="aspect-square rounded-xl object-cover"
           src="/galeria-4.jpg"
           alt="Membro da Igreja Batista da Trindade ajudando com a limpeza"
-          width={1000}
-          height={1000}
+          width={300}
+          height={300}
         />
       </div>
       <div className="grid max-w-xs gap-4">
@@ -35,22 +35,22 @@ export function ImageGallery() {
           priority
           src="/galeria-1.jpg"
           alt="Pastor Pedro lecionando na Escola Bíblica Dominical"
-          width={1000}
-          height={1000}
+          width={300}
+          height={300}
         />
         <Image
           className="aspect-square rounded-xl object-cover"
           src="/galeria-6.jpg"
           alt="A Bíblia Sagrada"
-          width={1000}
-          height={1000}
+          width={300}
+          height={300}
         />
         <Image
           className="aspect-square rounded-xl object-cover"
           src="/galeria-5.jpg"
           alt="Culto da Igreja Batista da Trindade"
-          width={1000}
-          height={1000}
+          width={300}
+          height={300}
         />
       </div>
     </section>
